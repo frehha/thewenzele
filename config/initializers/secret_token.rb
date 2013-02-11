0457a10aa20d477e09bd269810eb635b49bf9e10
frehha <christian--wenzel@web.de>
2013-02-11 18:53:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thewenzele::Application.config.secret_token = 'd603a987f23b4ba1dfda0d72b483159c7427fb5b0883b5dfcd19f9d5ab42f36e1de3429335b2b77b0e65f9d7d3e1984acda6cbe7c0fe9f7eddceaa3706abf5f7'
